692d0b1753217826d339f03bd8841afa19170690
daarani24 <daarani2007@gmail.com>
2026-09-08 22:52:58 +0530

    feat(deploy):configure url

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI(title="NammaVoice AI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://namma-voice-ai.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
